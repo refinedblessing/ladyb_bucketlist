e87dcc7f1dc2cafe0abd64f587d613e10248ecbf
Ebowe Blessing <blessing.ebowe@andela.com>
2015-11-21 14:14:11 +0100

    Removed empty spaces in doorkeeper migration file

--- db/migrate/20151119221200_create_doorkeeper_tables.rb
+++ db/migrate/20151119221200_create_doorkeeper_tables.rb
@@ -1,11 +1,11 @@
 class CreateDoorkeeperTables < ActiveRecord::Migration
   def change
     create_table :oauth_applications do |t|
-      t.string :name,         null: false
-      t.string :uid,          null: false
-      t.string :secret,       null: false
+      t.string :name, null: false
+      t.string :uid, null: false
+      t.string :secret, null: false
       t.text :redirect_uri, null: false
-      t.string :scopes,       null: false, default: ''
+      t.string :scopes, null: false, default: ''
       t.timestamps
     end
 
@@ -13,11 +13,11 @@ class CreateDoorkeeperTables < ActiveRecord::Migration
 
     create_table :oauth_access_grants do |t|
       t.integer :resource_owner_id, null: false
-      t.integer :application_id,    null: false
-      t.string :token,             null: false
-      t.integer :expires_in,        null: false
-      t.text :redirect_uri,      null: false
-      t.datetime :created_at,        null: false
+      t.integer :application_id, null: false
+      t.string :token, null: false
+      t.integer :expires_in, null: false
+      t.text :redirect_uri, null: false
+      t.datetime :created_at, null: false
       t.datetime :revoked_at
       t.string :scopes
     end
@@ -27,11 +27,11 @@ class CreateDoorkeeperTables < ActiveRecord::Migration
     create_table :oauth_access_tokens do |t|
       t.integer :resource_owner_id
       t.integer :application_id
-      t.text :token,             null: false
+      t.text :token, null: false
       t.string :refresh_token
       t.integer :expires_in
       t.datetime :revoked_at
-      t.datetime :created_at,        null: false
+      t.datetime :created_at, null: false
       t.string :scopes
     end
 
